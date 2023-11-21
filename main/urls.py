e573feb31477348ceb5f5c05386a21903b27bcc3
keishabella <anastasiakeishaa@gmail.com>
2023-11-22 04:23:19 +0700

    tugas 9

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
 from main.views import register, login_user, logout_user, edit_product, delete_product, increase, decrease
 from main.views import get_product_json, create_ajax
+from main.views import create_product_flutter
 
 app_name = 'main'
 
@@ -21,4 +22,5 @@ urlpatterns = [
     path('decrease/<int:id>/', decrease, name='decrease'),
     path('get-product/', get_product_json, name='get_product_json'),
     path('create-ajax/', create_ajax, name='create_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
